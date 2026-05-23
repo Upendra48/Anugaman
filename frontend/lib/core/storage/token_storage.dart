df4626d1ccb0475c2ea6302fd2ea4bcd8ff2b712
Upendra Raj Joshi <upendraofficial10@gmail.com>
2026-05-23 18:44:18 +0545

    Create: jwt token storage service

---
+++ frontend/lib/core/storage/token_storage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  static const _storage = const FlutterSecureStorage();
+
+  static const _key = "jwt_token";
+
+  static Future<void> saveToken(String token) async{
+    await _storage.write(key: _key, value: token);
+  }
+
+  static Future<String?> getToken() async{
+    return await _storage.read(key: _key);
+  }
+
+  static Future<void> clearToken() async{
+    await _storage.delete(key: _key);
+  }
+}
