2f5b686b18b8106681e453137c9e4129fb399f9d
Upendra Raj Joshi <upendraofficial10@gmail.com>
2026-05-23 18:40:56 +0545

    Create: dio client for central API client

---
+++ frontend/lib/core/api/dio_client.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import '../config/app_config.dart';
+
+class DioClient {
+  static final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: AppConfig.baseUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    )
+  );
+}
---
+++ frontend/lib/core/config/app_config.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppConfig{
+  static const String baseUrl = "http://localhost:8000/api";
+}
