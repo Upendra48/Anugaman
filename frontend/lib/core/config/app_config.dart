3bb523f145bd859e8c66650bede62b3bf382ec9e
Upendra Raj Joshi <upendraofficial10@gmail.com>
2026-05-25 15:01:49 +0545

    Add: flutter dependencies

--- frontend/lib/core/config/app_config.dart
+++ frontend/lib/core/config/app_config.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// This class contains the configuration for the application, such as the base URL for the API.
 class AppConfig{
   static const String baseUrl = "http://localhost:8000/api";
 }
